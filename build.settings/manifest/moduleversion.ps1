f9bbb3977860743fa03823301bb109febb7c947c
Joakim Olsson <joakim.olsson@addskills.com>
2020-02-13 10:14:47 +0100

    Removed the dynamic build number.
    It would not work well when developing/building in multiple locations at
    in parallel. Also extended the "build time" field to include hours,
    minutes and seconds of the day.

--- build.settings/manifest/moduleversion.ps1
+++ build.settings/manifest/moduleversion.ps1
@@ -1,11 +1,1 @@
-$buildFile = "$PSScriptRoot\meta\moduleversion.buildnumber"
-
-if (! (Test-Path $buildFile -PathType Leaf)) {
-    "0" > $buildFile
-}
-
-$build = cat $buildFile
-$build = [int]::Parse($build)
-$build++
-"0.1.{0}.{1:yyyyMMdd}" -f $build, [datetime]::now
-$build > $buildFile
+"0.1.0.{0:yyyyMMddHHmmss}" -f [datetime]::now
